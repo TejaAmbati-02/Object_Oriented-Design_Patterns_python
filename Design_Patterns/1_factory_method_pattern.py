bdf3412c73d8f71d856aa88c267b41a218e2e431
Teja <tejaambati20@gmail.com>
2026-07-02 00:32:39 +0530

    Add docstring to clarify the purpose of the Factory Method design pattern

--- Design_Patterns/1_factory_method_pattern.py
+++ Design_Patterns/1_factory_method_pattern.py
@@ -1,3 +1,5 @@
+# The Factory Method is a creational design pattern that provides an interface for creating objects in a superclass but allows subclasses to alter the type of objects that will be created.
+
 from abc import ABC, abstractmethod
 from enum import Enum
 
